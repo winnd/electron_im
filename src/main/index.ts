8f0e855f0c769179108b5858015b1bf0ff0c1a3c
winnd <AS40879@yeah.net>
2021-03-20 17:24:35 +0800

    增加vue-devtool

---
+++ src/main/index.ts
@@ -0,0 +1,43 @@
+import {app, BrowserWindow, session} from 'electron'
+// import {path} from 'node:path';
+import * as path from 'path'
+
+function createWindow() {
+    const windowConfig = {
+        width: 800,
+        height: 600,
+        webpreferences: {
+            nodeIntegration: true,
+        },
+    }
+    const win = new BrowserWindow(windowConfig)
+    win.loadURL('http://localhost:3020/')       // 静态地址 (可用于生产打包的时候)
+    win.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
+
+
+app.whenReady().then(() => {
+    session.defaultSession.loadExtension(
+        // 'C:/Users/winnd/AppData/Local/Google/Chrome/User Data/Default/Extensions/ljjemllljcmogpfapbkkighbhhppjdbg/vue-devtools-6.0.0.7_0',
+        path.resolve(__dirname, '../vue-devtools-6.0.0.7_0'),
+        {allowFileAccess: true}
+    ).catch((err) => {
+        debugger
+        console.error(err)
+    })
+})
+// C:\Users\winnd\Desktop\electron_im\vue-devtools-6.0.0.7_0
--- src/renderer/components/HelloWorld.vue
+++ src/renderer/components/HelloWorld.vue
@@ -1,27 +1,38 @@
 <template>
-    <h1>{{ msg }}</h1>
-    <div>asdf</div>
-    <img :src="img1" alt="">
+  <h1>{{ msg }}</h1>
+  <div>asdf</div>
+  <img :src="img1" alt="">
+  <textarea v-model="text" name="" id="" cols="30" rows="10"></textarea>
 </template>
 
 <script>
-import img1 from 'src/renderer/assert/img/1.png'
-import { defineComponent } from 'vue'
+import img1 from '../assert/img/1.png'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
-    name: 'HelloWord',
-    components: { img1 },
-    props: {
-        msg: String,
-    },
-    setup () {
-        console.log('setUp')
-    },
+  name      : 'HelloWord',
+  components: {img1},
+  props     : {
+    msg: String,
+  },
+  setup() {
+    const text = ref('')
+
+    return {
+      img1,
+      text,
+    }
+  },
 })
 </script>
-<style lang="scss">
-@import "../style/style";
+<style lang="scss" scoped>
+@import "../style/style.css";
+
 h1 {
-    background: orange;
+  background: orange;
+
+  div {
+    background: yellow;
+  }
 }
 </style>
