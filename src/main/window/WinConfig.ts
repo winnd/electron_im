790c8f28cf1c620fefc66fac53c82f308a37f812
winnd <AS40879@yeah.net>
2021-04-26 03:01:04 +0800

    (更新)更新窗口配置

--- src/main/window/WinConfig.ts
+++ src/main/window/WinConfig.ts
@@ -1,9 +1,20 @@
-import { BrowserWindowConstructorOptions, WebPreferences } from 'electron'
+import { BrowserWindow, BrowserWindowConstructorOptions, WebPreferences } from 'electron'
 
 export class WinConfig implements BrowserWindowConstructorOptions {
     width = 1000
     height = 600
+    // nativeWindowOpen todo 可能不需要
+    parent?: BrowserWindow
+    // thickFrame // todo 可能不需要
+    center: boolean = true
     webpreferences: WebPreferences = {
         nodeIntegration: true,
+        nodeIntegrationInSubFrames: true,
+        nodeIntegrationInWorker: true,
+        worldSafeExecuteJavaScript: true, // todo 可能不需要
+        contextIsolation: false, // todo 可能不需要
+        webSecurity: false, // 同源策略
+        disableHtmlFullscreenWindowResize: false, // todo 可能不需要
+        enableWebSQL: true,
     }
 }
