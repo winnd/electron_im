df7a1230598213567a0763f6e0da6295d4baaaa9
winnd <AS40879@yeah.net>
2021-04-27 02:32:22 +0800

    (修复) 修复eslint无法识别node属性方法问题

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,28 @@
 // eslint-disable-next-line no-undef
 module.exports = {
     parserOptions: {
-        parser: '@typescript-eslint/parser',
-        ecmaVersion: 2020,
-        sourceType: 'module',
+        parser      : '@typescript-eslint/parser',
+        ecmaVersion : 2020,
+        sourceType  : 'module',
         ecmaFeatures: {
             jsx: true
         }
     },
-    extends: [
+    env          : {
+        node   : true
+    },
+    extends      : [
         'eslint:recommended',
         'plugin:vue/vue3-recommended',
     ],
-    rules  : {
+    rules        : {
         // https://eslint.vuejs.org/rules/
         'vue/no-unused-vars'                        : 'error',
         'vue/no-unused-components'                  : 0,
         'vue/multiline-html-element-content-newline': 0,
         'vue/max-attributes-per-line'               : ['error', {
-            'singleline': {'max': 10},
-            'multiline' : {'max': 10}
+            'singleline': { 'max': 10 },
+            'multiline' : { 'max': 10 }
         }]
     }
 }
